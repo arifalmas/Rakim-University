e089e0011e9d2ddd6862eb81b2d2864a0c0ddc9c
arifalmas <freelancerarifar@gmail.comm>
2024-08-13 10:49:52 +0600

    updated

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Rakim University",
   url: "https://rakimuviversity.com",
-  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  description: "Establishment of the Definition of Science: The Emergence of Rakim University",
   author: "Saiful Islam Juboraj",
   links: {
     twitter: "https://twitter.com/juborajI",
