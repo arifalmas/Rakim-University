13c613bae4257f6882cb8f7516a6f885fcc51595
Arif Almas <98968872+arifalmas@users.noreply.github.com>
2024-09-09 13:25:38 +0600

    Update main-nav.tsx

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -18,10 +18,10 @@ export function MainNav() {
         href="/blog"
         className={cn(
           "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block",
-          pathname === "/Absolute Doctrine" ? "text-foreground" : "text-foreground/60"
+          pathname === "/blog" ? "text-foreground" : "text-foreground/60"
         )}
       >
-        Blog
+       My Doctrine
       </Link>
       <Link
         href="/about"
